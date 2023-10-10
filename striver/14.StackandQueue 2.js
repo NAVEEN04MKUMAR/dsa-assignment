35917cc9aff6bd462d78ffccd98fea81b2cbf355
NAVEEN04MKUMAR <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2023-10-10 20:05:45 +0530

    Create 14.StackandQueue 2.js

---
+++ striver/14.StackandQueue 2.js
@@ -0,0 +1,19 @@
+//Next Smaller Element				
+
+//LRU cache (IMPORTANT)		
+	
+//LFU cache		
+	
+//Largest rectangle in a histogram		
+	
+//Sliding Window maximum		
+	
+//Implement Min Stack		
+	
+//Rotten Orange (Using BFS)		
+	
+//Stock span problem				
+
+//Find the maximum of minimums of every wind…				
+
+//The Celebrity Problem
