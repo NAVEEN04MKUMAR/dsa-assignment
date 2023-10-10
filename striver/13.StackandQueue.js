864b73490fd6b2410fa61418ce30ebfed3ab6421
NAVEEN04MKUMAR <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2023-10-10 20:03:31 +0530

    Create 13.StackandQueue.js

---
+++ striver/13.StackandQueue.js
@@ -0,0 +1,13 @@
+//Implement Stack using Arrays		
+
+//Implement Queue using Arrays		
+
+//Implement Stack using Queue (using single …		
+	
+//Implement Queue using Stack (0(1) amortize…		
+	
+//Check for balanced parentheses		
+
+//Next Greater Element		
+	
+//Sort a Stack
