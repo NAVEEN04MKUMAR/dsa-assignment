7490b03095a998b2e058713fa4db7442b18b2b94
NAVEEN04MKUMAR <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2023-10-10 19:46:32 +0530

    Create 7.Linkedlist 3.js

---
+++ striver/7.Linkedlist 3.js
@@ -0,0 +1,11 @@
+//Rotate a LinkedList		
+	
+//Clone a Linked List with random and next p…		
+	
+//3 sum		
+	
+//Trapping Rainwater		
+	
+//Remove Duplicate from Sorted array		
+	
+//Max consecutive ones
