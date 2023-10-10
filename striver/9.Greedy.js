3cf97233d5cd4dbd29dfca77d44adcb7b4a8bad6
NAVEEN04MKUMAR <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2023-10-10 19:50:34 +0530

    Create 9.Greedy.js

---
+++ striver/9.Greedy.js
@@ -0,0 +1,11 @@
+//N meetings in one room	
+	
+//Minimum number of platforms required for a railway
+	
+//Job sequencing Problem		
+	
+//Fractional Knapsack Problem		
+	
+//Greedy algorithm to find minimum number of…		
+	
+//Activity Selection (it is the same as N meetings)
