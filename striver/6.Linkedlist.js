ab0044fa79090f14044f399d9a81210e6f4f2d66
NAVEEN04MKUMAR <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2023-10-10 19:42:42 +0530

    Create 6.Linkedlist.js

---
+++ striver/6.Linkedlist.js
@@ -0,0 +1,11 @@
+//Find intersection point of Y LinkedList		
+
+//Detect a cycle in Linked List		
+	
+//Reverse a LinkedList in groups of size k.		
+	
+//Check if a LinkedList is palindrome or not…		
+	
+//Find the starting point of the Loop of Lin…		
+	
+//Flattening of a LinkedList
