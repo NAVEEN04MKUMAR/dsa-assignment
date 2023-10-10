f489c13f673a1d72f4effe215dd29242d055637a
NAVEEN04MKUMAR <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2023-10-10 20:00:22 +0530

    Create 11.Binary search.js

---
+++ striver/11.Binary search.js
@@ -0,0 +1,15 @@
+//The N-th root of an integer		
+
+//Matrix Median		
+
+//Find the element that appears once in a so…		
+	
+//Search element in a sorted and rotated arr…		
+	
+//Median of 2 sorted arrays		
+	
+//K-th element of two sorted arrays		
+	
+//Allocate Minimum Number of Pages		
+
+//Aggressive Cows
