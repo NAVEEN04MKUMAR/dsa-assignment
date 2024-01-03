5d020567696d8af7bc2d6f233abc44ac00cc377f
NAVEEN04MKUMAR <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2024-01-03 10:01:21 +0530

    Update 17.binary trree.js

--- striver/17.binary trree.js
+++ striver/17.binary trree.js
@@ -1,5 +1,43 @@
 
 //Inorder Traversal		
+class Node{
+    constructor(value) {
+        this.data=value;
+        this.left=null;
+        this.right=null;
+    }
+}
+
+function inorder(node){
+    if (node=== null) {
+        return;
+    }
+    inorder(node.left);
+    console.log(node.data);
+    inorder(node.right);
+}
+
+ const root = new Node(1);
+ root.left= new Node(2);
+ root.right= new Node(3);
+ root.left.left= new Node(4);
+ root.left.right= new Node(5);
+ root.right.left= new Node(6);
+ root.right.right= new Node(7);
+ root.left.left.left= new Node(8);
+ root.left.left.right= new Node(9);
+ root.left.right.left= new Node(10);
+ root.left.right.right= new Node(11);
+ root.right.left.left= new Node(12);
+ root.right.left.right= new Node(13);
+ root.right.right.left= new Node(14);
+ root.right.right.right= new Node(15);
+
+console.log("inorder")
+inorder(root);
+
+
+
 	
 
 //Preorder Traversal		
