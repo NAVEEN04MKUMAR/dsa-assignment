6e659461ad987f6b865a0ff844f8605b21bd0018
NAVEEN04MKUMAR <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2024-01-18 11:04:37 +0530

    Update 18.binary trree 2.js

--- striver/18.binary trree 2.js
+++ striver/18.binary trree 2.js
@@ -1,10 +1,42 @@
-
-
-
-
-
 //Level order Traversal / Level order traversal of binary tree	
-	
+
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
+class Solution{
+    levelorder(root){
+        const queue=[];
+        const wraplist=[];
+
+        if(!root){
+            return wraplist;
+        }
+        queue.push(root);
+        while(queue.length>0){
+            //tp:push the left and right child of the current node get the answer
+            if(queue[0].left!==null){
+                queue.push(queue[0].left);
+            }
+            if(queue[0].right!==null){
+                queue.push(queue[0].right);
+            }
+            wraplist.push(queue.shift().val);
+        }     
+        return wraplist;
+    }
+}
+
+//Example usage:
+const solution = new Solution();
+const root = new TreeNode(3);
+root.left = new TreeNode(9);
+root.right = new TreeNode(20);
+root.right.left = new TreeNode(15);
+root.right.right = new TreeNode(7);
+console.log(solution.levelorder(root));
 
 //Height of a Binary Tree		
 	
