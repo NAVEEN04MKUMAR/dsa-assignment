8651bdab81d06cdd539a96d75773829e996f81cf
NAVEEN04MKUMAR <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2024-04-03 16:19:53 +0530

    Update 18.binary trree 2.js

--- striver/18.binary trree 2.js
+++ striver/18.binary trree 2.js
@@ -44,9 +44,93 @@ console.log(solution.levelorder(root));
 //Diameter of Binary Tree		
 	
 
-//Check if the Binary tree is height-balance…		
-	
+//Check if the Binary tree is height-balance…
+//approach #1
+//here we travelled the everynode nth time n nodes are available then we convert this one time complexity O(n2)
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
 
+class Solution{
+    isbalanced(root){
+        return this.dfsheight(root)!==-1;
+    }
+
+
+dfsheight(root){
+    if(root===null)return 0;
+
+    let leftheight=this.dfsheight(root.left);
+    if(leftheight===-1)
+    return -1;
+
+    let rightheight=this.dfsheight(root.right);
+    if(rightheight===-1)
+    return -1;
+   
+    //unbalanced if difference is morethan 1 from left and right subtree  
+    if(Math.abs(leftheight-rightheight>1))
+    return -1;
+
+    return Math.max(leftheight,rightheight)+1;
+    
+   }
+  }
+
+
+  
+//Example usage:
+const tree = new TreeNode(1);
+tree.left = new TreeNode(2);
+tree.right = new TreeNode(3);
+tree.left.left = new TreeNode(4);
+tree.left.right = new TreeNode(5);
+tree.right.right = new TreeNode(6);
+const solution = new Solution();
+console.log(solution.isbalanced(tree)); // Output: false
+
+
+//approach#2
+//approach #2
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
+
+class Solution{
+    isbalanced(root){
+        return this.dfsheight(root)!==-1;
+    }
+
+
+dfsheight(root){
+    if(root===null)return 0;
+
+    let leftheight=this.dfsheight(root.left);
+    let rightheight=this.dfsheight(root.right);
+
+    if(leftheight===-1||rightheight===-1||Math.abs(leftheight-rightheight)>1)
+     return -1;
+    return Math.max(leftheight,rightheight)+1;
+   }
+  }
+
+
+// Example usage:
+ const tree = new TreeNode(1);
+ tree.left = new TreeNode(2);
+ tree.right = new TreeNode(3);
+ tree.left.left = new TreeNode(4);
+ tree.left.right = new TreeNode(5);
+ tree.right.right = new TreeNode(6);
+ const solution2 = new Solution();
+ console.log(solution2.isbalanced(tree)); // Output: false
+	
 //LCA in Binary Tree		
 	
 
