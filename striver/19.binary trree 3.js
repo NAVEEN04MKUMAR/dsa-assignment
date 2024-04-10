2ccdaf1a7c17b182e89b677c4f9e4bc32249399b
NAVEEN04MKUMAR <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2024-04-10 12:00:01 +0530

    Update 19.binary tree 3.js

--- striver/19.binary trree 3.js
+++ striver/19.binary trree 3.js
@@ -49,9 +49,85 @@ const solution = new Solution();
 const maxPathSum = solution.maxpathsum(root);
 console.log("Maximum Path Sum:", maxPathSum);
 
+
+
+
+
+
 //construct a binary tree from a inorder and preorder
+class Treenode{
+    constructor(val){
+        this.val=val;
+        this.left=null;
+        this.right=null;
+    }
+}
 
 
+class Solution {
+    buildtree(preorder,inorder){
+        //store inorder in the map
+        const inmap=new Map();
+        //populate the map with indices of elements in the inorder traversal
+        inorder.forEach((val,index) => {
+            inmap.set(val,index);
+        });
+
+        const root=this.buildtreehelper(preorder,0,preorder.length-1,inorder,0,inorder.length-1,inmap);
+        return root;
+
+    }
+    buildtreehelper(preorder,prestart,preend,inorder,instart,inend,inmap){
+        if(prestart>preend||instart>inend){
+            return null;
+        }
+        //
+        const root=new Treenode(preorder[prestart]);
+        //find index
+        const inroot=inmap.get(root.val);
+        //count number of left subtree
+        const numsleft=inroot-instart;
+
+//build the left and right subtree        
+        root.left=this.buildtreehelper(preorder,prestart+1,prestart+numsleft,inorder,instart,inroot-1,inmap);
+        root.right=this.buildtreehelper(preorder,prestart+numsleft+1,preend,inorder,inroot+1,inend,inmap);
+        return root;
+    }
+}
+//verify the after construct the tree get the inorder 
+function printinorder(root){
+    if(!root){
+        return;
+    }
+    printinorder(root.left);
+    console.log(root.val+" ");
+    printinorder(root.right);
+}
+
+function printarray(array){
+    array.forEach((element)=>{
+        console.log(element+"");
+    });
+    console.log();
+}
+
+
+function main(){
+    const inorder=[9,3,15,20,7];
+    const preorder=[3,9,20,15,7];
+
+    console.log("inorder array:");
+    printarray(inorder);
+
+    console.log("preorder array:");
+    printarray(preorder);
+
+    const sol=new Solution();
+    const root=sol.buildtree(preorder,inorder);
+    console.log("inorder of unique binary tree created:");
+    printinorder(root);    
+}
+main();
 
 //construct a binary tree from a inorder and postorder
 
