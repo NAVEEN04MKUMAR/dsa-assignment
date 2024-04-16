68ca46ae82b9f2169611db47e3f456e4064e1b4b
NAVEEN04MKUMAR <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2024-04-16 16:19:56 +0530

    Update 21.binarysearch tree2.js

--- striver/21.binarysearchtree2.js
+++ striver/21.binarysearchtree2.js
@@ -28,7 +28,6 @@ return floor;
 }
 
 
-// Example usage:
 let root = new TreeNode(8);
 root.left = new TreeNode(4);
 root.right = new TreeNode(12);
@@ -39,3 +38,49 @@ root.right.right = new TreeNode(14);
 
 let key = 5;
 console.log("Floor of", key, "is", floor(root, key)); 
+
+//ceil of value
+//floor value in the bst
+class TreeNode{
+    constructor(val){
+        this.val=val;
+        this.left=null;
+        this.right=null;
+    }
+}
+
+function ceil(root,key){
+    let ceil=null;
+
+    while(root){
+        if(root.val===key){
+            return root.val;
+        }
+    
+    else if(root.val<key){
+        root=root.right;
+    }
+    else{
+        ceil=root.val;
+        root=root.left;
+    }
+
+}
+return ceil;
+}
+
+
+// Example usage:
+let root = new TreeNode(8);
+root.left = new TreeNode(4);
+root.right = new TreeNode(12);
+root.left.left = new TreeNode(2);
+root.left.right = new TreeNode(6);
+root.right.left = new TreeNode(10);
+root.right.right = new TreeNode(14);
+
+let key = 5;
+console.log("ceil of", key, "is",ceil(root, key)); 
+
+
+
