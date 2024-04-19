a26cca8768d1d022cb70e34dc7edc87a6eb795b4
NAVEEN04MKUMAR <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2024-04-19 17:03:37 +0530

    Update 21.binarysearchtree2.js

--- striver/21.binarysearchtree2.js
+++ striver/21.binarysearchtree2.js
@@ -79,8 +79,93 @@ root.left.right = new TreeNode(6);
 root.right.left = new TreeNode(10);
 root.right.right = new TreeNode(14);
 
+
+
 let key = 5;
 console.log("ceil of", key, "is",ceil(root, key)); 
+
+
+//find kth smallest and largest element in the tree
+class TreeNode {
+    constructor(x) {
+        this.val = x; 
+        this.left = null; 
+        this.right = null; 
+    }
+}
+
+
+class Solution{
+    reverseorder(node,counter,k,klargest){
+        if (!node || counter >= k) return;
+        this.reverseorder(node.right,counter,k ,klargest);
+        counter++;//increment counter after visiting right subtree
+        if(counter===k){
+            klargest[0]=node.val;
+            return;
+        }
+        this.reverseorder(node.left,counter,k ,klargest);
+    }
+
+    inorder(node,counter,k,ksmallest){
+        if (!node || counter >= k) return;
+        this.inorder(node.left,counter,k ,ksmallest);
+        counter++;//increment counter after visiting left subtree
+        if(counter===k){
+            ksmallest[0]=node.val;
+            return;
+        }
+        this.inorder(node.right,counter,k ,ksmallest);
+    }
+
+    findkth(root,k){
+        const ksmallest=[Number.MIN_SAFE_INTEGER];
+        const klargest=[Number.MIN_SAFE_INTEGER];
+        let counter=0;
+        this.inorder(root,counter,k ,ksmallest);
+        counter=0;
+        this.reverseorder(root,counter,k ,klargest);
+        return [ksmallest[0],klargest[0]];
+    }
+}
+
+
+
+function printInOrder(root) {
+    if (root === null) {
+        return;
+    }
+    printInOrder(root.left);
+    console.log(root.val + " ");
+    printInOrder(root.right);
+}
+
+// Creating a binary search tree (BST)
+const root = new TreeNode(10);
+root.left = new TreeNode(5);
+root.right = new TreeNode(13);
+root.left.left = new TreeNode(3);
+root.left.left.left = new TreeNode(2);
+root.left.left.right = new TreeNode(4);
+root.left.right = new TreeNode(6);
+root.left.right.right = new TreeNode(9);
+root.right.left = new TreeNode(11);
+root.right.right = new TreeNode(14);
+
+console.log("Binary Search Tree:");
+printInOrder(root);
+console.log();
+
+// Instantiate the Solution class
+const solution = new Solution();
+
+// Find the Kth smallest and largest elements
+const k = 3;
+console.log("k: " + k);
+const kthElements = solution.findkth(root, k);
+
+console.log("Kth smallest element: " + kthElements[0]);
+console.log("Kth largest element: " + kthElements[1]);
 
 
 
