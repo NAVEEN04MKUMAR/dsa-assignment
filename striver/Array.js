cb960a200ef3b08d9496e9ff4db82bc0f166b168
NAVEEN04MKUMAR <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2024-08-06 14:26:13 +0100

    Update 1.Array.js

--- striver/Array.js
+++ striver/Array.js
@@ -382,6 +382,93 @@ o/p:
 3 2 1
 3 1 2
 
+//Find maximum sum then 
+//Brute force method
+
+function maxSubarraySum(arr, n) {
+    let maxi = Number.MIN_SAFE_INTEGER; // maximum sum
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            // subarray = arr[i.....j]
+            let sum = 0;
+
+            //add all the elements of subarray:
+            for (let k = i; k <= j; k++) {
+                sum += arr[k];
+            }
+
+            maxi = Math.max(maxi, sum);
+        }
+    }
+
+    return maxi;
+}
+
+const arr = [ -2, 1, -3, 4, -1, 2, 1, -5, 4];
+const n = arr.length;
+const maxSum = maxSubarraySum(arr, n);
+console.log(`The maximum subarray sum is: ${maxSum}`);
+
+//better solution
+
+function maxSubarraySum(arr, n) {
+    let maxi = Number.MIN_SAFE_INTEGER; // maximum sum
+
+    for (let i = 0; i < n; i++) {
+        let sum = 0;
+        for (let j = i; j < n; j++) {
+            // current subarray = arr[i.....j]
+
+            //add the current element arr[j]
+            // to the sum i.e. sum of arr[i...j-1]
+            sum += arr[j];
+
+            maxi = Math.max(maxi, sum); // getting the maximum
+        }
+    }
+
+    return maxi;
+}
+
+let arr = [ -2, 1, -3, 4, -1, 2, 1, -5, 4];
+let n = arr.length;
+let maxSum = maxSubarraySum(arr, n);
+console.log("The maximum subarray sum is: " + maxSum);
+
+//optimal solution
+
+function maxSubarraySum(arr, n) {
+    let maxi = Number.MIN_SAFE_INTEGER; // maximum sum
+    let sum = 0;
+
+    for (let i = 0; i < n; i++) {
+        sum += arr[i];
+
+        if (sum > maxi) {
+            maxi = sum;
+        }
+
+        // If sum < 0: discard the sum calculated
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+
+    // To consider the sum of the empty subarray
+    // uncomment the following check:
+
+    //if (maxi < 0) maxi = 0;
+
+    return maxi;
+}
+
+const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+const n = arr.length;
+const maxSum = maxSubarraySum(arr, n);
+console.log("The maximum subarray sum is: " + maxSum);
+
+
 
 
 
