7589fa451f07dda1ab545acbebc97d5d6eb94d38
NAVEEN04MKUMAR <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2024-09-18 11:08:15 +0530

    Update github-javascript.js

--- github-javascript.js
+++ github-javascript.js
@@ -18,3 +18,47 @@ console.log("object",obj2);
 //we can get the all the key value pair by console the obj2
 
 
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Try programiz.pro");
+
+//#3 objet create method
+//#3 create the new object using the already existing object
+
+let exp1={
+    what:'prepare',
+    branch:'mechanical',
+    score:'disqualified'
+}
+
+
+let exp2={
+ prop1: {
+    value: 'value1',
+    writable: true,
+    enumerable: true,
+    configurable: true
+  },
+  prop2: {
+    value: 'value2',
+    writable: false,
+    enumerable: true,
+    configurable: false
+  }    }
+
+var final=Object.create(exp1,exp2);
+console.log("final",final.what);
+console.log("final",final.branch);
+console.log("final",final.score);
+console.log("final",final.prop1);
+console.log("final",final.prop2);
+
+//from this if we passs the exp2 as a argument directly then we will get the object it show type error
+//if we give access then it show the object
+//if we access the exp1 only the objectname we will not get the value 
+//if we through the ke value then get the value 
+
+
+
+
