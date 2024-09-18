da8df2b2dbd1b6177a8ee031eb4b077e480eb047
NAVEEN04MKUMAR <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2024-09-18 10:42:30 +0530

    Update github-javascript.js

--- github-javascript.js
+++ github-javascript.js
@@ -1,1 +1,20 @@
 
+1.What are the ways i can create the object:
+
+//#1 object literatural way
+var object={
+name:"naveen",
+age:"24"
+};
+console.log("object",object);
+
+//#2:new object constructor
+const obj2=new Object();
+obj2.key1="value1";
+obj2.key2="value2";
+obj2.key3="value3";
+
+console.log("object",obj2);
+//we can get the all the key value pair by console the obj2
+
+
