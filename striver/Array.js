8eb292eef6ae498637cca7099119f472fd6a22d1
Naveenkumar M <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2024-10-15 14:07:06 +0530

    Update Array.js

--- striver/Array.js
+++ striver/Array.js
@@ -497,7 +497,34 @@ console.log(arr.join(" "));
 //tc:o(n);
 //sc:(1);
 
+//Best Time to Buy and Sell Stock
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+//You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+//Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
+//Input: prices = [7,1,5,3,6,4]
+//Output: 5
+///Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+//Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+    function maxProfit(prices) {  
+    let max=0;
+    console.log('max',max);
+    for(let i=0;i<prices.length;i++){
+    for(let j=i+1;j<prices.length;j++){
+        if(prices[j]>prices[i]){
+            max=Math.max(prices[j]-prices[i],max);
+        }
+
+    }
+};
+return max;
+}
+let arr=[7,1,5,3,6,4];
+let maxp=maxProfit(arr);
+console.log("Max profit is: " + maxp);
+//t.c:O(n^2);
+//s.c(1);
 
 
 
