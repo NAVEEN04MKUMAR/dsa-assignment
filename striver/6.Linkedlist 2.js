dfc2d99b9ab54dc75983843dcfc4cf183d75ce5e
Naveenkumar M <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2024-10-27 14:00:54 +0530

    Update 6.Linkedlist 2.js

--- striver/6.Linkedlist 2.js
+++ striver/6.Linkedlist 2.js
@@ -1,7 +1,49 @@
 //Find intersection point of Y LinkedList		
 
-//Detect a cycle in Linked List		
-	
+//Detect a cycle in Linked List	
+class node{
+    constructor(data){
+        this.data=data;
+        this.next=null;
+    }
+}
+function hasCycle(head){
+    let current=head;
+    const nodemap=new Map();
+    while(current!==null){
+       //if node already in map return true
+        if(nodemap.has(current)){
+        return true;
+        }
+//to te map not already added add now
+        nodemap.set(current,true);
+//move the pointer
+        current=current.next;
+    }
+    return false;
+}
+
+let head=new node(1);
+let node2=new node(1);
+let node3=new node(1);
+let node4=new node(1);
+let node5=new node(1);
+let node6=new node(1);
+let node7=new node(1);
+let node8=new node(2);
+
+head.next=node2;
+node2.next=node3;
+node3.next=node4;
+node4.next=node5;
+node5.next=node2;
+node6.next=node7;
+node7.next=node8;
+
+
+let updatedHead = hasCycle(head);
+//t.c:total number of node searches i have the o(N) and the insertion and the searching of the node at the worst time complexity:O(nlogn),finally o(2nlogn)
+	 
 //Reverse a LinkedList in groups of size k.		
 	
 //Check if a LinkedList is palindrome or not…		
