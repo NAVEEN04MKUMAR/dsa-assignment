15dd74a344c3da49b8192e40152995d759c168b5
Naveenkumar M <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2024-11-09 20:59:59 +0530

    Update 4.Array.js

--- striver/4.Array.js
+++ striver/4.Array.js
@@ -1,5 +1,99 @@
 
 
+function countPairs(a, n) {
+  // Count the number of pairs:
+  let cnt = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if (a[i] > 2 * a[j]) cnt++;
+    }
+  }
+  return cnt;
+}
+
+function team(skill, n) {
+  return countPairs(skill, n);
+}
+
+let a = [4, 1, 2, 3, 1];
+let n = 5;
+let cnt = team(a, n);
+console.log("The number of reverse pair is: " + cnt);
+
+//Time Complexity: O(N2), 
+//Time Complexity: O(2N*logN)
+
+
+function merge(arr, low, mid, high) {
+  let temp = []; // temporary array
+  let left = low; // starting index of left half of arr
+  let right = mid + 1; // starting index of right half of arr
+
+  // storing elements in the temporary array in a sorted manner
+  while (left <= mid && right <= high) {
+    if (arr[left] <= arr[right]) {
+      temp.push(arr[left]);
+      left++;
+    } else {
+      temp.push(arr[right]);
+      right++;
+    }
+  }
+
+  // if elements on the left half are still left
+  while (left <= mid) {
+    temp.push(arr[left]);
+    left++;
+  }
+
+  // if elements on the right half are still left
+  while (right <= high) {
+    temp.push(arr[right]);
+    right++;
+  }
+
+  // transferring all elements from temporary to arr
+  for (let i = low; i <= high; i++) {
+    arr[i] = temp[i - low];
+  }
+}
+
+function countPairs(arr, low, mid, high) {
+  let right = mid + 1;
+  let cnt = 0;
+  for (let i = low; i <= mid; i++) {
+    while (right <= high && arr[i] > 2 * arr[right]) right++;
+    cnt += right - (mid + 1);
+  }
+  return cnt;
+}
+
+function mergeSort(arr, low, high) {
+  let cnt = 0;
+  if (low >= high) return cnt;
+  let mid = Math.floor((low + high) / 2);
+  cnt += mergeSort(arr, low, mid); // left half
+  cnt += mergeSort(arr, mid + 1, high); // right half
+  cnt += countPairs(arr, low, mid, high); // Modification
+  merge(arr, low, mid, high); // merging sorted halves
+  return cnt;
+}
+
+function team(skill, n) {
+  return mergeSort(skill, 0, n - 1);
+}
+
+let a = [4, 1, 2, 3, 1];
+let n = 5;
+let cnt = team(a, n);
+console.log("The number of reverse pair is: " + cnt);
+
+
+//4 Sum | Find Quads that add up to a target value
+//Problem Statement: Given an array of N integers, your task is to find unique quads that add up to give a target value. 
+//In short, you need to return an array of all the unique quadruplets [arr[a], arr[b], arr[c], arr[d]] such that their sum is equal to a given target.
+
+
 
 function fourSum(nums, target) {
   let n = nums.length;
