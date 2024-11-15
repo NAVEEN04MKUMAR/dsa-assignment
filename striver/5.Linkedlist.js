781ebf2d3affa9c707daf549e6ba02340199daad
Naveenkumar M <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2024-11-15 11:28:52 +0530

    Update 5.Linkedlist.js

--- striver/5.Linkedlist.js
+++ striver/5.Linkedlist.js
@@ -1,5 +1,178 @@
 //Reverse a LinkedList		
-	
+
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+function reverseList(head) {
+  let temp = head; 
+  const stack = []; 
+
+  // Step 1: Push the values of the linked list onto the stack
+  while (temp !== null) {
+    stack.push(temp.data); 
+    temp = temp.next; 
+  }
+
+  temp = head; // Reset temp to the head of the linked list
+
+  // Step 2: Pop values from the stack and update the linked list
+  while (temp !== null) {
+    temp.data = stack.pop(); 
+    temp = temp.next; 
+  }
+
+  return head; 
+}
+
+
+function printLinkedList(head) {
+  let temp = head;
+  let result = [];
+  while (temp !== null) {
+    result.push(temp.data);
+    temp = temp.next;
+  }
+  console.log(result.join(" ")); 
+}
+
+
+let head = new Node(1);
+head.next = new Node(3);
+head.next.next = new Node(2);
+head.next.next.next = new Node(4);
+
+
+console.log("Original Linked List:");
+printLinkedList(head);
+
+head= reverseList(head);
+// console.log(result)
+
+console.log("Reversed Linked List:");
+printLinkedList(head);
+
+//Time Complexity: O(2N) This is because we traverse the linked list twice: once to push the values onto the stack, and once to pop the values and update the linked list. 
+//Both traversals take O(N) time, hence time complexity  O(2N) ~ O(N).
+//Space Complexity: O(N) We use a stack to store the values of the linked list, and in the worst case, the stack will have all N values, 
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+// Function to reverse a linked list
+// using the 3-pointer approach
+function reverseList(head) {
+  
+    let temp = head;  
+    let prev = null;  
+    while (temp !== null) {  
+        let front = temp.next;  
+        // Reverse direction of current node's
+        // 'next' pointer to point to 'prev'
+        temp.next = prev;  
+        
+        // Move 'prev' to the current node,
+        // preparing it for the next iteration
+        prev = temp;
+        
+        // Move 'temp' to the 'front' node
+        // (the next node), advancing traversal
+        temp = front;  
+    }
+    return prev;  
+}
+
+
+function printLinkedList(head) {
+  let temp = head;
+  while (temp !== null) {
+    process.stdout.write(temp.data + ' ');
+    temp = temp.next;
+  }
+  console.log();
+}
+
+
+const head = new Node(1);
+head.next = new Node(3);
+head.next.next = new Node(2);
+head.next.next.next = new Node(4);
+
+process.stdout.write('Original Linked List: ');
+printLinkedList(head);
+
+reverseList(head);
+
+process.stdout.write('Reversed Linked List: ');
+printLinkedList(head);
+//Time Complexity: O(N) The code traverses the entire linked list once, where 'n' is the number of nodes in the list. This traversal has a linear time complexity, O(n).
+//Space Complexity: O(1) The code uses only a constant amount of additional space, regardless of the linked list's length
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+function reverseList(head) {
+
+    if (head === null || head.next === null) {
+        return head;
+    }
+    var newHead = reverseList(head.next);
+    // Save a reference to the node following
+    // the current 'head' node.
+    var front = head.next;
+    
+    // Make the 'front' node point to the current
+    // 'head' node in the reversed order.
+    front.next = head;
+    
+    // Break the link from the current 'head' node
+    // to the 'front' node to avoid cycles.
+    head.next = null;
+    
+    // Return the 'newHead,' which is the new
+    // head of the reversed linked list.
+    return newHead;
+}
+
+function printLinkedList(head) {
+  let temp = head;
+  while (temp !== null) {
+    process.stdout.write(temp.data + ' ');
+    temp = temp.next;
+  }
+  console.log();
+}
+
+const head = new Node(1);
+head.next = new Node(3);
+head.next.next = new Node(2);
+head.next.next.next = new Node(4);
+
+process.stdout.write('Original Linked List: ');
+printLinkedList(head);
+
+reverseList(head);
+
+process.stdout.write('Reversed Linked List: ');
+printLinkedList(head);
+
+//Time Complexity: O(N) This is because we traverse the linked list twice: once to push the values onto the stack, and once to pop the values and update the linked list.
+//Both traversals take O(N) time.
+//Space Complexity : O(1) No additional space is used explicitly for data structures or allocations during the linked list reversal process.
+//However, it's important to note that there is an implicit use of stack space due to recursion. 
+
 //Find the middle of LinkedList		
 	
 //Merge two sorted Linked List (use method u…		
