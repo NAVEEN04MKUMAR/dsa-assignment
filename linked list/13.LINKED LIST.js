27432270ec9c4198e9a2e336f31f0ecb4a676882
Naveenkumar M <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2024-11-19 18:42:47 +0530

    Update 13.LINKED LIST.js

--- linked list/13.LINKED LIST.js
+++ linked list/13.LINKED LIST.js
@@ -1,187 +1,3 @@
-status:do3,4,5,6,7,8
-# Assignment Questions 13
-
-<aside>
-💡 **Question 1**
-
-Given two linked list of the same size, the task is to create a new linked list using those linked lists. The condition is that the greater node among both linked list will be added to the new linked list.
-
-**Examples:**
-
-```
-Input: list1 = 5->2->3->8
-list2 = 1->7->4->5
-Output: New list = 5->7->4->8
-
-Input:list1 = 2->8->9->3
-list2 = 5->3->6->4
-Output: New list = 5->8->9->4
-```
-//define the datastructure
-class node{
-    constructor(data){
-        this.data=data;
-        this.next=null;
-    }
-}
-function createnewlist(head1,head2){
-    let pointer1=head1;
-    let pointer2=head2;
-    let newhead=null;
-
-while(pointer1!==null&&pointer2!==null){
-let newnode=new node();
-//when the any one of the linkedlist is null 
-if(pointer1 !== null && (pointer2 === null || pointer1.data <= pointer2.data)){
-    newnode.data=pointer2.data;
-    pointer2=pointer2.next;
-}else if(pointer2 !== null && (pointer1 === null || pointer1.data > pointer2.data)){
-    newnode.data=pointer1.data;
-    pointer1=pointer1.next;
-}
-//else{
-// //compare the values from the both the linkedlist which one is bigger we need to take them
-// //also skip the current element
-// if(pointer1.data>pointer2.data){
-//     newnode.data=pointer1.data;
-//     pointer1=pointer1.next;
-// }
-// else{
-//     newnode.data=pointer2.data;
-//     pointer2=pointer2.next;
-// }
-}
-    
-        // If newhead is null, initialize it
-        if (newhead === null) {
-            newhead = newnode;
-            tail = newnode;
-        } else {
-            tail.next = newnode;
-            tail = newnode;
-//         }
-    
-// newnode.next=newhead;
-// newhead=newnode;
-}
-return newhead;
-}
-//create the linkedlist1
-
-let head1=new node(1);
-let node2=new node(2);
-let node3=new node(3);
-let node4=new node(4);
-//connecet the linked list
-head1.next=node2;
-node2.next=node3;
-node3.next=node4;
-
-//create the linkedlist2
-let head2=new node(5);
-let node6=new node(6);
-let node7=new node(7);
-let node8=new node(8);
-
-//connecet the linkedlist2
-head2.next=node6;
-node6.next=node7;
-node7.next=node8;
-
-let newhead = createnewlist(head1, head2);
-let current = newhead;
-while (current !== null) {
-  console.log(current.data);
-  current = current.next;
-}
-
-</aside>
-
-<aside>
-💡 **Question 2**
-
-Write a function that takes a list sorted in non-decreasing order and deletes any duplicate nodes from the list. The list should only be traversed once.
-
-For example if the linked list is 11->11->11->21->43->43->60 then removeDuplicates() should convert the list to 11->21->43->60.
-
-**Example 1:**
-
-```
-Input:
-LinkedList: 
-11->11->11->21->43->43->60
-Output:
-11->21->43->60
-```
-
-**Example 2:**
-
-```
-Input:
-LinkedList: 
-10->12->12->25->25->25->34
-Output:
-10->12->25->34
-```
-//
-class node{
-    constructor(data){
-        this.data=data;
-        this.next=null;
-    }
-}
-function removeduplicate(head){
-    let current=head;
-    while(current!==null&&current.next!==null){
-//system:if the current and next element are equal then the directly jumped into after next element
-        if(current.data===current.next.data){
-            current.next=current.next.next;
-        }else{
-            current=current.next;
-        }
-    }
-    return head;
-}
-
-let head=new node(1);
-let node2=new node(1);
-let node3=new node(1);
-let node4=new node(1);
-let node5=new node(1);
-let node6=new node(1);
-let node7=new node(1);
-let node8=new node(2);
-
-head.next=node2;
-node2.next=node3;
-node3.next=node4;
-node4.next=node5;
-node5.next=node6;
-node6.next=node7;
-node7.next=node8;
-
-
-// Remove duplicates
-let updatedHead = removeduplicate(head);
-
-// Traverse and print the updated linked list
-let current = updatedHead;
-while (current !== null) {
-  console.log(current.data);
-  current = current.next;
-}
-
-</aside>
-
-
-
-
-
-
-
-
-
-
 
 <aside>
 💡 **Question 3**
@@ -304,3 +120,112 @@ Output:5 2 9
 ```
 
 </aside>
+
+💡 **Question 4**
+
+Given a singly linked list of characters, write a function that returns true if the given list is a palindrome, else false.
+
+!https://media.geeksforgeeks.org/wp-content/uploads/20220816144425/LLdrawio.png
+
+**Examples:**
+
+> Input: R->A->D->A->R->NULL
+> 
+> 
+> **Output:** Yes
+> 
+> **Input:** C->O->D->E->NULL
+> 
+> **Output:** No
+> 
+</aside>
+
+<aside>
+💡 **Question 5**
+
+Given a linked list of **N** nodes such that it may contain a loop.
+
+A loop here means that the last node of the link list is connected to the node at position X(1-based index). If the link list does not have any loop, X=0.
+
+Remove the loop from the linked list, if it is present, i.e. unlink the last node which is forming the loop.
+
+**Example 1:**
+
+```
+Input:
+N = 3
+value[] = {1,3,4}
+X = 2
+Output:1
+Explanation:The link list looks like
+1 -> 3 -> 4
+     ^    |
+     |____|
+A loop is present. If you remove it
+successfully, the answer will be 1.
+
+```
+
+**Example 2:**
+
+```
+Input:
+N = 4
+value[] = {1,8,3,4}
+X = 0
+Output:1
+Explanation:The Linked list does not
+contains any loop.
+```
+
+**Example 3:**
+
+```
+Input:
+N = 4
+value[] = {1,2,3,4}
+X = 1
+Output:1
+Explanation:The link list looks like
+1 -> 2 -> 3 -> 4
+^              |
+|______________|
+A loop is present.
+If you remove it successfully,
+the answer will be 1.
+```
+
+</aside>
+
+<aside>
+💡 **Question 6**
+
+Given a linked list and two integers M and N. Traverse the linked list such that you retain M nodes then delete next N nodes, continue the same till end of the linked list.
+
+Difficulty Level: Rookie
+
+**Examples**:
+
+```
+Input:
+M = 2, N = 2
+Linked List: 1->2->3->4->5->6->7->8
+Output:
+Linked List: 1->2->5->6
+
+Input:
+M = 3, N = 2
+Linked List: 1->2->3->4->5->6->7->8->9->10
+Output:
+Linked List: 1->2->3->6->7->8
+
+Input:
+M = 1, N = 1
+Linked List: 1->2->3->4->5->6->7->8->9->10
+Output:
+Linked List: 1->3->5->7->9
+```
+
+</aside>
+
+<aside>
