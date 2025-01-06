7fefa7d014778bdb121a396faaeb108a268f74eb
Naveenkumar M <124698445+NAVEEN04MKUMAR@users.noreply.github.com>
2025-01-06 23:47:12 +0530

    Update 15.string.java

--- striver/15.string.java
+++ striver/15.string.java
@@ -139,3 +139,77 @@ class Main {
         return right-left-1;
     }
 }
+
+
+
+//686. Repeated String Match
+// Given two strings a and b, return the minimum number of times you should repeat string a so that string b is a substring of it. If it is impossible for b​​​​​​ to be a substring of a after repeating it, return -1.
+// Notice: string "abc" repeated 0 times is "", repeated 1 time is "abc" and repeated 2 times is "abcabc".
+
+// Example 1:
+// Input: a = "abcd", b = "cdabcdab"
+// Output: 3
+// Explanation: We return 3 because by repeating a three times "abcdabcdabcd", b is a substring of it.
+
+
+// class Solution {
+//     public int repeatedStringMatch(String a, String b) {
+        
+//         StringBuilder repeated=new StringBuilder();
+//         int count=0;
+
+//         while(repeated.length()<=a.length()+b.length()){
+//             if(repeated.toString().contains(b)){
+//                 return count;
+//             }
+
+//            repeated.append(a);
+//             count++;
+//         }
+// return -1;
+//     }
+//     public static void main(String[] args){
+//         Solution solution=new Solution();
+
+//          System.out.println(solution.repeatedStringMatch("abcd", "cdabcdab")); 
+//         System.out.println(solution.repeatedStringMatch("efgh", "iefghe")); 
+
+
+//     }
+// }
+
+
+
+
+class Solution {
+  public int repeatedStringMatch(String a, String b) {
+  int repeated=(int) Math.ceil((double) b.length()/a.length());
+System.out.println(repeated); 
+  StringBuilder repeat=new StringBuilder();
+System.out.println(repeat); 
+
+  // Append `a` `repeated` times to the StringBuilder
+    for (int i = 0; i < repeated; i++) {
+      repeat.append(a);
+    }
+
+            if(repeat.toString().contains(b)){
+                return repeated;
+            }
+
+           repeat.append(a);
+
+             if(repeat.toString().contains(b)){
+                return repeated+1;
+            }
+return -1;
+    }
+    public static void main(String[] args){
+        Solution solution=new Solution();
+
+         System.out.println(solution.repeatedStringMatch("abcd", "cdabcdab")); 
+        System.out.println(solution.repeatedStringMatch("efgh", "iefghe")); 
+    }
+}
+
+
